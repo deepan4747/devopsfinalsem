722369b777af57d8dd342ee8edafc8cac93e50a7
deepan4747 <127740629+deepan4747@users.noreply.github.com>
2025-05-13 14:42:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 docker build -t task3 .
 echo Hyyy
 docker login -u deepan47 -p Deepan11032004
-docker tag task3 deepan47/devopsfinalsem
-docker push deepan47/devopsfinalsem
+docker tag task3 deepan47/devopsguvi
+docker push deepan47/devopsguvi
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
